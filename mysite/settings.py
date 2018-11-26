26702e7bbc5ce7f120d00f3c8d754fb7c95f3f5b
manu <kamrahimanshu08@gmail.com>
2018-11-26 08:35:32 +0530

    changed the html for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g5x=@sbmq26#e8wsds^k0+@20#j^71q_$qj3pz)g0!%l^m3+_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG=True
 
 ALLOWED_HOSTS = []
 
