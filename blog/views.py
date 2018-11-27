9beef958b6b816ce59fd43db19792ee22f3f9b7d
manu <kamrahimanshu08@gmail.com>
2018-11-27 07:50:41 +0530

    added view and template for detailed

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 # noinspection PyUnresolvedReferences
+from blog.models import Post
 from .models import Post
 # noinspection PyUnresolvedReferences
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
